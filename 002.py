6d76e7f242534594a6e8315366d9b60784b04888
Denis <chekmarevdenchik5@gmail.com>
2022-07-19 13:40:20 +0700

    002.py is done

--- 002.py
+++ 002.py
@@ -4,3 +4,16 @@
 
 """
 
+def do_set(some_list: list) -> list:
+    result = []
+    if some_list == []:
+        return []
+    for i in some_list:
+        if i not in result:
+            result.append(i)
+    return result
+
+
+origin_list = [1, 3, 1, 5, 3, 6, 5]
+result_list = do_set(origin_list)
+print(result_list)
