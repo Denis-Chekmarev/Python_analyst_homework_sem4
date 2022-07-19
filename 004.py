fb07b6ce34df136de16c815df7c3d18197ba0285
Denis <chekmarevdenchik5@gmail.com>
2022-07-19 14:38:48 +0700

    004.py is done

--- 004.py
+++ 004.py
@@ -6,3 +6,27 @@
 
 """
 
+
+def is_word(word: str) -> bool:
+    numbers = [str(i) for i in range(10)]
+    for symbol in word:
+        if symbol in numbers:
+            return False
+    return True
+
+
+def get_words(text: str) -> str:
+    list = text.split(' ')
+    result = ''
+    for word in list:
+        if is_word(word):
+            result += word + ' '
+    return result
+
+
+with open('file.txt', encoding='utf-8') as file:
+    s = ''
+    for line in file:
+        s += line
+
+print(get_words(s))
